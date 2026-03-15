a5612f05d1c7a9e07b3fb7e071e8919a15b3f17a
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-15 18:07:53 +0530

    Added dto module and created employeeDTO class

---
+++ src/main/java/org/sanjeet/springbootweb/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package org.sanjeet.springbootweb.dto;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+
+
+}
