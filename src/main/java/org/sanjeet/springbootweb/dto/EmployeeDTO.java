bb47cf5e14ce80ad368809fc8c215514643c18f7
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-15 18:10:44 +0530

    added getter and setter to expose the private field of employeeDTO class

--- src/main/java/org/sanjeet/springbootweb/dto/EmployeeDTO.java
+++ src/main/java/org/sanjeet/springbootweb/dto/EmployeeDTO.java
@@ -9,5 +9,43 @@ public class EmployeeDTO {
     private Integer age;
     private LocalDate dateOfJoining;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
 }
