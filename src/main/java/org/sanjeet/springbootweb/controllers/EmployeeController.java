944b44779a257336fdbb673c862d42adbf456167
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-15 18:29:54 +0530

    added controllers module and EmployeeController class to handle incoming api request from the client. And added one get mapping controller method in order to handle the random url

---
+++ src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
@@ -0,0 +1,12 @@
+package org.sanjeet.springbootweb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/employees/*")
+    public String message(){
+        return "Incorrect URL, please go back and hit correct URL";
+    }
+}
