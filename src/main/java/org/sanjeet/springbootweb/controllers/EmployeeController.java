883146925a38a11f34a077122c5e5db9faafcc38
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-15 18:33:05 +0530

    renamed the controller method named message to handleRandomURL signifying the corrct name for it functionality

--- src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
+++ src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
     @GetMapping("/employees/*")
-    public String message(){
+    public String handleRandomURL(){
         return "Incorrect URL, please go back and hit correct URL";
     }
 }
