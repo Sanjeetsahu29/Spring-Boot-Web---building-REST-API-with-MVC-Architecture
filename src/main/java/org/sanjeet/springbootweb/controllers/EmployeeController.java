0e78e09f1a5fcba1b33d4a52879b54f93d23efba
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-15 18:43:51 +0530

    added getEmployeeById controller method to return the hard coded new employee with the provided ID

--- src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
+++ src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
@@ -1,12 +1,21 @@
 package org.sanjeet.springbootweb.controllers;
 
+import org.sanjeet.springbootweb.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
     @GetMapping("/employees/*")
     public String handleRandomURL(){
         return "Incorrect URL, please go back and hit correct URL";
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+        return new EmployeeDTO(employeeId, "Sanjeet Sahu", "sanjeetsahus29@gmail.com", 28, LocalDate.of(2026, 2, 22), true);
     }
 }
