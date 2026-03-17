8dd34b4860d481c1368030d0766d6cc66042caad
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-17 14:21:28 +0530

    added path=employees in RequestMapping to advoid repeatation of the employees in each path of diffrent HTTP method

--- src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
+++ src/main/java/org/sanjeet/springbootweb/controllers/EmployeeController.java
@@ -7,23 +7,24 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
     private final EmployeeRepository employeeRepository;
     public EmployeeController(EmployeeRepository employeeRepository){
         this.employeeRepository = employeeRepository;
     }
 
-    @GetMapping("/employees/*")
-    public String handleRandomURL(){
-        return "Incorrect URL, please go back and hit correct URL";
-    }
+//    @GetMapping("/*")
+//    public String handleRandomURL(){
+//        return "Incorrect URL, please go back and hit correct URL";
+//    }
 
-    @GetMapping("/employees/{employeeId}")
+    @GetMapping("/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
         return new EmployeeDTO(employeeId, "Sanjeet Sahu", "sanjeetsahus29@gmail.com", 28, LocalDate.of(2026, 2, 22), true);
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/")
     public String getMessageWithQueryParameter(@RequestParam(required = false) Integer age,
                                               @RequestParam(required = false) String sortBy){
         return "Hard coded Query parameter are age: "+age+" and SortBy filter method: "+sortBy;
@@ -31,18 +32,18 @@ public class EmployeeController {
     // Since this is the controller method of type post, so you can't be able to hit this
     // api request from the browser url. We need a client that can mimic our frontend client
     // Using POSTMAN we can make api request of different HTTP methods
-    @PostMapping("/employees")
+    @PostMapping("/")
     public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployeeInfo){
         inputEmployeeInfo.setId(100L);
         return inputEmployeeInfo;
     }
 
-    @PutMapping("/employees")
+    @PutMapping("/")
     public String updateEmployee(){
         return "Hello from the Put controller method";
     }
 
-    @DeleteMapping("/employees")
+    @DeleteMapping("/")
     public String deleteEmployee(){
         return "Hello from the Delete controller method";
     }
