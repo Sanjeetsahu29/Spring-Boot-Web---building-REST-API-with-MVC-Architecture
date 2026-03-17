b682c2bcaa9d57761063113a3e30bf2521dc3307
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-17 14:09:38 +0530

    added the Repository annotation which extend component and it becomes a bean. Now it's injection will be handled by IoC container

--- src/main/java/org/sanjeet/springbootweb/repositories/EmployeeRepository.java
+++ src/main/java/org/sanjeet/springbootweb/repositories/EmployeeRepository.java
@@ -2,7 +2,9 @@ package org.sanjeet.springbootweb.repositories;
 
 import org.sanjeet.springbootweb.entities.EmployeeEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntities, Long> {
 
 }
