15582520eb6710bde5159da4f82b4fd6441b447c
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-18 11:54:05 +0530

    instead creating mapper object manually,I have created a class called MapperConfig and one method which return a object of ModelMapper for dependency injection

---
+++ src/main/java/org/sanjeet/springbootweb/config/MapperConfig.java
@@ -0,0 +1,11 @@
+package org.sanjeet.springbootweb.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+public class MapperConfig {
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
