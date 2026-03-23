b56cf84f7a0a914d0af6dec121530cc3893d495b
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-23 23:52:14 +0530

    reformat the code for proper indentation of the code

--- src/main/java/org/sanjeet/springbootweb/dto/EmployeeDTO.java
+++ src/main/java/org/sanjeet/springbootweb/dto/EmployeeDTO.java
@@ -3,6 +3,7 @@ package org.sanjeet.springbootweb.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +17,18 @@ import java.time.LocalDate;
 
 public class EmployeeDTO {
     private Long id;
+
     @NotBlank(message = " name field can neither be null nor empty")
+    @Size(min = 3, max = 50, message = "Number of characters in name should be in the range [3,50]")
     private String name;
+
     @Email
     private String email;
+
     private Integer age;
+
     private LocalDate dateOfJoining;
+
     @JsonProperty("isActive")
     private Boolean isActive;
 
