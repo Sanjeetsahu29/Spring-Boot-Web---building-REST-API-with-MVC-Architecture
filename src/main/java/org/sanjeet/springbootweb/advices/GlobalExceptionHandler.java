23977f131d5411c6c5ca654f9ada7048e321b4bf
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2026-03-28 10:25:59 +0530

    added a exception handler for Method argument is not valid

--- src/main/java/org/sanjeet/springbootweb/advices/GlobalExceptionHandler.java
+++ src/main/java/org/sanjeet/springbootweb/advices/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package org.sanjeet.springbootweb.advices;
 import org.sanjeet.springbootweb.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,5 +29,18 @@ public class GlobalExceptionHandler {
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationError(MethodArgumentNotValidException exception){
+        List<String> errors = exception.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(errors.toString())
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
